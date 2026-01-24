8a73d2a5d36d3e091899620fb98958be50dd2e18
Arsen Kuatuly <050526501387@enu.kz>
2026-01-24 12:19:43 +0500

    Фикс resetprogress v2

--- utils/resetProgress.js
+++ utils/resetProgress.js
@@ -1,11 +1,18 @@
 
-const db = require("../lib/db");
+async function resetAllProgress(db, userId) {
 
+    if (db && typeof db.query !== "function") {
+        throw new Error("resetAllProgress: first argument must be db with .query()");
+    }
 
-async function resetAllProgress(userId) {
-    await db.query("DELETE FROM user_lesson_progress WHERE user_id = $1", [userId]);
-    await db.query("DELETE FROM user_module_progress WHERE user_id = $1", [userId]);
-    await db.query("DELETE FROM user_course_progress WHERE user_id = $1", [userId]);
+    const uid = Number(userId);
+    if (!Number.isInteger(uid) || uid <= 0) {
+        throw new Error(`resetAllProgress: invalid userId = ${JSON.stringify(userId)}`);
+    }
+
+    await db.query("DELETE FROM user_lesson_progress WHERE user_id = $1", [uid]);
+    await db.query("DELETE FROM user_module_progress WHERE user_id = $1", [uid]);
+    await db.query("DELETE FROM user_course_progress WHERE user_id = $1", [uid]);
 }
 
 module.exports = { resetAllProgress };
