fb72f74abec07917c98cfce4d452be2a1d758df2
Arsen Kuatuly <050526501387@enu.kz>
2026-01-24 12:15:42 +0500

    Фикс resetprogress

--- utils/resetProgress.js
+++ utils/resetProgress.js
@@ -1,7 +1,11 @@
-async function resetAllProgress(db, userId) {
-    await db.execute(`DELETE FROM user_lesson_progress WHERE user_id = ?`, [userId]);
-    await db.execute(`DELETE FROM user_module_progress WHERE user_id = ?`, [userId]);
-    await db.execute(`DELETE FROM user_course_progress WHERE user_id = ?`, [userId]);
+
+const db = require("../lib/db");
+
+
+async function resetAllProgress(userId) {
+    await db.query("DELETE FROM user_lesson_progress WHERE user_id = $1", [userId]);
+    await db.query("DELETE FROM user_module_progress WHERE user_id = $1", [userId]);
+    await db.query("DELETE FROM user_course_progress WHERE user_id = $1", [userId]);
 }
 
 module.exports = { resetAllProgress };
